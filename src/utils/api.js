efe62cb8afd9a61e623db5d63a996c6651c4bbfc
arch0615 <cb8906485@gmail.com>
2026-03-25 09:51:15 +0000

    vercel env

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = '/api/pdf';
+const API_BASE = `${import.meta.env.VITE_API_URL || ''}/api/pdf`;
 
 export const getTemplateInfo = () => axios.get(`${API_BASE}/template-info`);
 
